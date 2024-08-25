f8742374db33b2508262adf18403d0f088532694
krish736 <112505529+krish736@users.noreply.github.com>
2024-08-25 23:34:12 +0530

    commit

---
+++ Code-Bin/src/app/components/signup/signup.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+
+  constructor(private route:Router){}
+
+  register(regForm: NgForm){
+    console.log(regForm.value);
+    this.route.navigate(['/login'])
+  }
+
+  reset(regForm: NgForm){
+    regForm.reset()
+  }
+}
